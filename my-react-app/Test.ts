766c514af778f5ac14d4207c8f315cf25929e28d
AntoLima <antonellalimarieri9@gmail.com>
2026-09-04 11:23:51 -0300

    Delete my-react-app directory

--- my-react-app/Test.ts
+++
@@ -1,7 +0,0 @@
-const nombrepersona:string = "Pepe"
-const apellidopersona:string = "Perez"
-const edadpersona:number = 30
-
-console.log(`Hola, mi nombre es ${nombrepersona} y mi apellido es ${apellidopersona} y mi edad es ${edadpersona}`)
-
-//Hola, mi nombre es {} +" "+ apellido +"   
--- my-react-app/test.js
+++
@@ -1,5 +0,0 @@
-var nombre = "Pepe";
-var apellido = "Perez";
-var edad = 30;
-console.log("Hola, mi nombre es ".concat(nombre, " y mi apellido es ").concat(apellido, " y mi edad es ").concat(edad));
-//Hola, mi nombre es {} +" "+ apellido +"  
